4cd94bb63f983a5a923059b34a9e9af537a419e0
KarlaM16 <karlamingaherrera@gmail.com>
2024-06-26 09:35:22 -0500

    ca
    cambios

--- application/controllers/Area.php
+++ application/controllers/Area.php
@@ -4,7 +4,11 @@ defined('BASEPATH')|| exit('No direct script access allowed');
 class Area extends CI_Controller
 {
 
- 
+  public function __construct()
+  {
+    parent::__construct();
+    
+  }
 
   public function index()
   {
